6dacc56398d6103190417c5b3baa9e037c25a1be
Kauan Campos <97120244+kauancampos@users.noreply.github.com>
2025-05-26 15:39:21 -0300

    Add files via upload
    expo init

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './src/pages/HomePage'
+
+const App = () => {
+  return(
+    <HomePage/>
+  );
+
+}
+
+export default App
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+  return(
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text> PUC MINAS </Text> 
+    </View>
+  );
+
+}
+
+export default HomePage
